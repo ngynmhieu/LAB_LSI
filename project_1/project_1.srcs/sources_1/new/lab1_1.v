2f826997941ecd45035888dffb38a75d72887777
ngynmhieu <ngyn.mhieu@gmail.com>
2024-03-01 10:35:49 +0700

    Mostly finished

--- project_1/project_1.srcs/sources_1/new/lab1_1.v
+++ project_1/project_1.srcs/sources_1/new/lab1_1.v
@@ -29,7 +29,7 @@ module Bound_Flash(
 
     reg [3:0] state;
     reg [3:0] max, min;
-    reg [3:0] counter;
+    reg [4:0] counter;
     reg [4:0] lampstate;
     reg [4:0] kickback;
     
@@ -44,70 +44,71 @@ module Bound_Flash(
        state = INIT;
    end else begin // Reset = 1
        case (state)
-       INIT: begin  // initial state
-           max = 4'b0000;
-           min = 4'b0000;
-           lamps = 16'b0;
-           lampstate = 0;
-           kickback = 0;
-           if (flick == 1'b1) state = ON_STATE;
+       INIT: begin
+        max = 0;
+        min = 0;
+        counter = 0 ;
+        kickback = 0;
+        lamps = 16'b0;
+        lampstate = 1;
+        if (flick == 1) state = ON_STATE;
        end
 
-       ON_STATE: begin //set up max and min for ON
-           state = GRADUALLY_ON;
-           if (lampstate == 0) begin
-               max = 5; min = 0;
-           end else if (lampstate == 2) begin
-               max = 10; min = 0;
-           end else if (lampstate == 4) begin
-               max = 15; min = 5;
-           end else if (lampstate == 6) begin
-               lamps = 16'b1;
-               state = OFF_STATE;
-               lampstate = lampstate + 1;
-           end
-           counter = min;
+       ON_STATE: begin
+        state = GRADUALLY_ON;
+        if (lampstate == 1) begin
+            min = 0; max = 5;
+        end else if (lampstate == 3) begin
+            min = 0 ; max = 10;
+        end else if (lampstate == 5) begin
+            min = 5; max = 15;
+        end else if (lampstate == 7) begin
+            lamps = 16'b1111111111111111;
+            state = OFF_STATE;
+        end
+        counter = min;
        end
 
        GRADUALLY_ON: begin
-           if (flick == 1 && (lampstate == 2 || lampstate == 4)) begin // kickback point
-               kickback = 1;
-               lampstate = lampstate - 1;
-               state = OFF_STATE;
-           end else begin
-                   lamps[counter] = 1'b1;
-                   counter = counter + 1;
-                   if (counter > max ) begin
-                    state = OFF_STATE;
-                    lampstate = lampstate + 1;
-                   end
-           end
+        if (flick == 1 && (counter == 5 || counter == 10) && lampstate != 1) begin
+            kickback = 1;
+            lampstate = lampstate -1 ;
+            state = OFF_STATE;
+        end else begin
+            lamps[counter] = 1'b1;
+            if (counter == max) begin
+                state = OFF_STATE;
+                lampstate = lampstate + 1;
+            end
+            counter = counter + 1;
+        end
        end
 
-       OFF_STATE: begin //set up max and min for OFF
-           state = GRADUALLY_OFF;
-           if (lampstate == 1) begin
-               max = 6; min = 1;
-           end else if (lampstate == 3) begin
-               max = 11; min = 6;
-           end else if (lampstate == 5) begin
-               max = 16; min = 1;
-           end else if (lampstate == 7) begin
-               lamps = 16'b0;
-               state = INIT;
-           end
-           if (kickback == 0) counter = max;
-           else kickback = 0;
+       OFF_STATE: begin
+        state = GRADUALLY_OFF;
+        if (lampstate == 2) begin
+            max = 5; min = 0;
+        end else if (lampstate == 4) begin
+            max = 10; min = 5;
+        end else if (lampstate == 6) begin
+            max = 15; min = 0;
+        end else if (lampstate == 7) begin
+            lamps = 16'b0;
+            state = INIT;
+        end
+        if (kickback == 0) counter = max;
+        else kickback = 0;
        end
 
        GRADUALLY_OFF: begin
-            counter = counter - 1;
-            lamps[counter] = 1'b0;
-            if (counter < min) begin
-               state = ON_STATE;
-               lampstate = lampstate + 1;
-            end
+        lamps [counter] = 1'b0;
+        if (counter == min) begin
+            state = ON_STATE;
+            lampstate = lampstate + 1;
+        end
+        counter = counter - 1;
        end
        endcase
    end
 endmodule
+
